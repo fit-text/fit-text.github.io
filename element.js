38d448d8aeae3f671e5de17d06595728e755a58b
Danny Engelman <danny@engelman.nl>
2023-07-13 18:39:50 +0200

    v9

--- element.js
+++ element.js
@@ -1,17 +1,20 @@
 customElements.define("fit-text", class extends HTMLElement {
     constructor() {
         super().attachShadow({ mode: "open" }).innerHTML = `<slot style=display:inline-block />` +
-            `<style>:host{display:inline-block;width:100%;white-space:nowrap}</style>`
+            `<style>:host{display:inline-block;width:100%;white-space:nowrap`
     }
     connectedCallback(// define variables and functions as parameters to save LET declarations
         slot = this.shadowRoot.querySelector("slot"),
         // FUNCTION resizes the text to fit the <fit-text> element
-        fit = () => setTimeout(() => this.style.fontSize = parseInt(getComputedStyle(slot).fontSize) * (this.clientWidth / slot.scrollWidth) + "px"),
+        fit = () => setTimeout(() =>
+            this.style.fontSize =
+            parseInt(getComputedStyle(slot).fontSize) *
+            (this.clientWidth / slot.scrollWidth) + "px"),
         // FUNCTION add listener; return removeEventListener function
-        event = (name, _ = addEventListener(name, fit)) => () => removeEventListener(name, fit)
+        event = (root, name, _ = root.addEventListener(name, fit)) => () => root.removeEventListener(name, fit)
     ) {
-        this.r = event("resize") // fit when the window resizes
-        this.l = event("loadingdone") // fit when fonts load
+        this.r = event(window, "resize") // fit when the window resizes
+        this.l = event(document.fonts, "loadingdone") // fit when fonts load
         fit() // fit the text on first load
     }
     disconnectedCallback() {
