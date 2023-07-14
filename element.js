20be7a9dcfac8b5713e1fa727f7ff7248265c6fd
Danny Engelman <danny@engelman.nl>
2023-07-14 09:49:19 +0200

    v1

--- element.js
+++ element.js
@@ -5,13 +5,10 @@ customElements.define("fit-text", class extends HTMLElement {
     }
     connectedCallback(// define variables and functions as parameters to save LET declarations
         slot = this.shadowRoot.querySelector("slot"),
-        // FUNCTION resizes the text to fit the <fit-text> element
-        fit = () => setTimeout(() =>
-            this.style.fontSize =
-            parseInt(getComputedStyle(slot).fontSize) *
-            (this.clientWidth / slot.scrollWidth) + "px"),
+        // FUNCTION fit text to fit the <fit-text> element
+        fit = x => setTimeout(x => this.style.fontSize = parseInt(getComputedStyle(slot).fontSize) * (this.clientWidth / slot.scrollWidth) + "px"),
         // FUNCTION add listener; return removeEventListener function
-        event = (root, name, _ = root.addEventListener(name, fit)) => () => root.removeEventListener(name, fit)
+        event = (root, name, x = root.addEventListener(name, fit)) => x => root.removeEventListener(name, fit)
     ) {
         this.r = event(window, "resize") // fit when the window resizes
         this.l = event(document.fonts, "loadingdone") // fit when fonts load
