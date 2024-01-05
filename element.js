e46f71c6ee9f89a148d090a252a0bae964a71091
Danny Engelman <danny@engelman.nl>
2024-01-05 13:23:17 +0100

    added MutationObserver

--- element.js
+++ element.js
@@ -1,21 +1,37 @@
 customElements.define("fit-text", class extends HTMLElement {
     constructor() {
-        super().attachShadow({ mode: "open" }).innerHTML = `<slot style=display:inline-block>` +
+        super()
+            .attachShadow({ mode: "open" })
+            .innerHTML = `<slot style=display:inline-block>` +
             `<style>:host{display:inline-block;width:100%;white-space:nowrap`
     }
     connectedCallback(// define variables and functions as parameters to save LET declarations
         slot = this.shadowRoot.querySelector("slot"),
         // FUNCTION fit text to fit the <fit-text> element
-        fit = x => setTimeout(x => this.style.fontSize = parseInt(getComputedStyle(slot).fontSize) * (this.clientWidth / slot.scrollWidth) + "px"),
+        fit = _ =>
+            setTimeout(
+                _ => this.style.fontSize = parseInt(getComputedStyle(slot).fontSize) * (this.clientWidth / slot.scrollWidth) + "px"
+            ),
         // FUNCTION add listener; return removeEventListener function
         event = (root, name, x = root.addEventListener(name, fit)) => x => root.removeEventListener(name, fit)
     ) {
-        this.r = event(window, "resize") // fit when the window resizes
-        this.l = event(document.fonts, "loadingdone") // fit when fonts load
+        if (!this.hasAttribute("static")) {
+        // Listeners outside 'this' need to be removed on disconnect! Keep a reference
+            this.r = event(window, "resize") // fit when the window resizes
+            this.l = event(document.fonts, "loadingdone") // fit when fonts load
+            //slotchange Listener will be garbage collected!
+            event(slot, "slotchange")
+
+            // MutationObserver for all lightDOM changes
+            this.o = new MutationObserver(fit);
+            this.o.observe(this, { childList: 1, subtree: 1, characterData: 1 })
+        }
+
         fit() // fit the text on first load
     }
     disconnectedCallback() {
         this.r() // remove "resize" listener
         this.l() // remove font "loadingdone" listener
+        this.o.disconnect();
     }
 });
