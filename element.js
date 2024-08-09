a775fbe6ab12ba58657499869615f341db093385
Danny Engelman <danny@engelman.nl>
2024-08-09 11:59:14 +0200

    <fit-text> Web Component

--- element.js
+++ element.js
@@ -2,8 +2,8 @@ customElements.define("fit-text", class extends HTMLElement {
     constructor() {
         super()
             .attachShadow({ mode: "open" })
-            .innerHTML = `<slot style=display:inline-block>` +
-            `<style>:host{display:inline-block;width:100%;white-space:nowrap`
+            .innerHTML = "<slot style=display:inline-block>" +
+            "<style>:host{display:inline-block;width:100%;white-space:nowrap"
     }
     connectedCallback(// define variables and functions as parameters to save LET declarations
         slot = this.shadowRoot.querySelector("slot"),
@@ -14,6 +14,7 @@ customElements.define("fit-text", class extends HTMLElement {
             ),
         // FUNCTION add listener; return removeEventListener function
         event = (root, name, x = root.addEventListener(name, fit)) => x => root.removeEventListener(name, fit),
+        resizeevent, loadingdoneevent,
         observers = [] //array of MutationObservers
     ) {
         fit() // fit the text on first load
@@ -21,8 +22,8 @@ customElements.define("fit-text", class extends HTMLElement {
         // watch for any (any!) DOM changes, unless the "static" attribute is set
         if (!this.hasAttribute("static")) {
             // Listeners outside 'this' need to be removed on disconnect! Keep a reference
-            this.r = event(window, "resize") // fit when the window resizes
-            this.l = event(document.fonts, "loadingdone") // fit when fonts load
+            resizeevent = event(window, "resize") // fit when the window resizes
+            loadingdoneevent = event(document.fonts, "loadingdone") // fit when fonts load
 
             slot.addEventListener("slotchange", (e) => {
                 // watch new Child elements with shadowRoots, attach MutationObserver
@@ -36,11 +37,15 @@ customElements.define("fit-text", class extends HTMLElement {
             // MutationObserver for all <fit-text> lightDOM changes
             observers.push(this.observer = new MutationObserver(fit))
             this.observer.observe(this, { childList: true, subtree: true, characterData: true })
+
+            this.disconnect = () => {
+                resizeevent() // remove "resize" listener
+                loadingdoneevent() // remove font "loadingdone" listener
+                observers.map(x => x.disconnect()) // remove all MutationObservers
+            }
         }
     }
     disconnectedCallback() {
-        this.r() // remove "resize" listener
-        this.l() // remove font "loadingdone" listener
-        observers.map(x => x.disconnect()) // remove all MutationObservers
+        this.disconnect && this.disconnect() // remove listeners and observers
     }
 });
